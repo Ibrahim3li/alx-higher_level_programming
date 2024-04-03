92f2ec167ca44e7260678804fa37946d38eb2fbd
ibrahim3li <ibrahim3li.9619@gmail.com>
2024-04-03 16:00:59 -0700

    edit

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# 0-square.py 
+# 0-square.py by Ehoneah Obed
 """A module that defines a square """
 
 
@@ -42,4 +42,4 @@ class Square:
         Returns: The square of the size
         """
 
-    return (self.__size ** 2
+        return (self.__size ** 2)
