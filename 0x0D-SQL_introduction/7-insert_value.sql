5bd8646aa7d081d5d673d32b6946fc774d543c2f
ibrahim3li <ibrahim3li.9619@gmail.com>
2024-05-24 00:54:21 +0300

    edit

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,4 +1,4 @@
 --Inserts a new row into first_table in the database hbtn_0c_0
 -- New row: id = 89, name = Best School
 
-INSERT INTO first_table (id, name) values (89, 'Best School');
+INSERT INTO first_table (id, name) VALUES (89, 'Best School');
